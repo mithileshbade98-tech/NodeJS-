3de1f6bf7aee3a0ddfc8d0b6965fa94a8630bc0b
mithileshbade98-tech <mbade@hawk.iit.edu>
2023-10-29 19:22:18 -0500

    Basic JS Changes

---
+++ play.js
@@ -0,0 +1,11 @@
+var personname = 'Mithilesh';
+var age = 25;
+var hobbies = true;
+
+function myself(userName, Userage, Userhobbies) {
+    return (
+        'Name is ' + userName + ', age is ' + Userage + ' and hobbies is ' + Userhobbies
+    );
+}
+
+console.log(myself(personname, age, hobbies));
